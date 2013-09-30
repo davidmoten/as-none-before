5c4b06f3e6a92381dd5c35eaa65d68dd2fed1bef
Dave Moten <davidmoten@gmail.com>
2013-09-30 22:29:04 +1000

    tests

--- asn1-parser/src/test/java/com/github/davidmoten/asn1/CompilerTest.java
+++ asn1-parser/src/test/java/com/github/davidmoten/asn1/CompilerTest.java
@@ -21,4 +21,13 @@ public class CompilerTest {
 		c.compile();
 	}
 
+	@Test
+	public void testBigger2() {
+		for (File f : new File("src/main/asn1/x420").listFiles()) {
+			System.out.println(f);
+			Compiler c = new Compiler(Collections.singletonList(f));
+			c.compile();
+		}
+	}
+
 }
