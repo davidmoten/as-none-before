91df321a5a4248ae0f45a6e086bc9b7bc060b3f1
Dave Moten <davidmoten@gmail.com>
2016-07-21 12:37:53 +1000

    use antlr4 and enhance grammar a bit, not working though

--- asn1-parser/src/main/java/com/github/davidmoten/asn1/Compiler.java
+++ asn1-parser/src/main/java/com/github/davidmoten/asn1/Compiler.java
@@ -4,76 +4,99 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import org.antlr.runtime.ANTLRFileStream;
-import org.antlr.runtime.CharStream;
-import org.antlr.runtime.CommonTokenStream;
-import org.antlr.runtime.ParserRuleReturnScope;
-import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.tree.CommonTree;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+import org.antlr.v4.runtime.tree.RuleNode;
 
-import com.github.davidmoten.asn1.ASNParser.moduleDefinition_return;
-import com.github.davidmoten.asn1.Asn1Parser.moduleDefinitions_return;
+import com.github.davidmoten.asn1.ASNParser.ModuleDefinitionContext;
 
 public class Compiler {
 
-	private final List<File> files;
+    private final List<File> files;
 
-	public Compiler(List<File> files) {
-		this.files = files;
-	}
+    public Compiler(List<File> files) {
+        this.files = files;
+    }
 
-	public void compile() {
-		for (File file : files) {
-			try {
-				CharStream s = new ANTLRFileStream(file.getPath());
+    // public void compile() {
+    // for (File file : files) {
+    // try {
+    // CharStream s = new ANTLRFileStream(file.getPath());
+    //
+    // Asn1Lexer lexer = new Asn1Lexer(s);
+    // CommonTokenStream tokens = new CommonTokenStream(lexer);
+    //
+    // Asn1Parser parser = new Asn1Parser(tokens);
+    //
+    // moduleDefinitions_return defs = parser.moduleDefinitions();
+    // process(defs, 0);
+    // } catch (IOException e) {
+    // throw new RuntimeException(e);
+    // } catch (RecognitionException e) {
+    // e.printStackTrace();
+    // }
+    // }
+    // }
 
-				Asn1Lexer lexer = new Asn1Lexer(s);
-				CommonTokenStream tokens = new CommonTokenStream(lexer);
+    public void compile() {
+        for (File file : files) {
+            try {
+                CharStream s = new ANTLRFileStream(file.getPath());
 
-				Asn1Parser parser = new Asn1Parser(tokens);
+                ASNLexer lexer = new ASNLexer(s);
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-				moduleDefinitions_return defs = parser.moduleDefinitions();
-				process(defs, 0);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			} catch (RecognitionException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+                ASNParser parser = new ASNParser(tokens);
 
-	public void compile2() {
-		for (File file : files) {
-			try {
-				CharStream s = new ANTLRFileStream(file.getPath());
+                ModuleDefinitionContext defs = parser.moduleDefinition();
+                process(defs, 1);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (RecognitionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-				ASNLexer lexer = new ASNLexer(s);
-				CommonTokenStream tokens = new CommonTokenStream(lexer);
+    private void process(ModuleDefinitionContext r, final int maxDepth) {
+        ParseTreeVisitor<Object> visitor = new AbstractParseTreeVisitor<Object>() {
 
-				ASNParser parser = new ASNParser(tokens);
+            int depth = 0;
 
-				moduleDefinition_return defs = parser.moduleDefinition();
-				process(defs, 0);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			} catch (RecognitionException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+            @Override
+            public Object visit(ParseTree tree) {
+                return tree.accept(this);
+            }
 
-	private void process(ParserRuleReturnScope r, int depth) {
-		process((CommonTree) r.getTree(), depth);
-	}
+            @Override
+            public Object visitChildren(RuleNode node) {
+                // System.out.println(node.getText());
+                depth += 1;
+                if (depth <= maxDepth) {
+                    return node.accept(this);
+                } else {
+                    return node;
+                }
+            }
 
-	private void process(CommonTree t, int depth) {
-		for (int i = 0; i < depth; i++)
-			System.out.print("  ");
-		System.out.println(t.getText() + ":" + t.getType());
-		if (t.getChildCount() > 0)
-			for (Object c : t.getChildren()) {
-				process((CommonTree) c, depth + 1);
-			}
-	}
+        };
+        r.accept(visitor);
+    }
+
+    private void process(CommonTree t, int depth) {
+        for (int i = 0; i < depth; i++)
+            System.out.print("  ");
+        System.out.println(t.getText() + ":" + t.getType());
+        if (t.getChildCount() > 0)
+            for (Object c : t.getChildren()) {
+                process((CommonTree) c, depth + 1);
+            }
+    }
 
 }
